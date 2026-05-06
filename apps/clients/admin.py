92aee3d055caaf998cd6ad38c050ea247a5a13b1
Francisco Rivas <fran.workbench@gmail.com>
2026-05-05 20:55:16 -0400

    ADD chapter 2

---
+++ apps/clients/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from .models import Client
+
+@admin.register(Client)
+class ClientAdmin(admin.ModelAdmin):
+    list_display = ('nombre', 'cedula', 'codigo_afiliado', 'fecha_ingreso')
+    search_fields = ('nombre', 'cedula', 'codigo_afiliado')
+    list_filter = ('fecha_ingreso',)
